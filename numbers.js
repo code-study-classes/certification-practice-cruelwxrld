0f9a997fda9e1232db82b5eaae9bfa366b4d0eb8
Дмитрий <cruelwxrlddd@gmail.com>
2023-11-03 13:53:15 +0300

    Выполнение заданий 1-10

--- numbers.js
+++ numbers.js
@@ -80,25 +80,18 @@ export const getDayOfWeek = (dayOfYear) => {
     switch (dayOfYear % 7) {
         case 1:
             return 1;
-            break;
         case 2:
             return 2;
-            break;
         case 3:
             return 3;
-            break;
         case 4:
             return 4;
-            break;
         case 5:
             return 5;
-            break;
         case 6:
-            return 6;
-            break;                
+            return 6;               
         default:
             return 0;
-            break;
     }
 };
 
